afce10fce4badbd7ef6ac4bece9626be2c23bde5
Andrey Gorobtzov <92875957+AndreyGorobtzov@users.noreply.github.com>
2023-11-11 18:55:56 +0500

    Update CComputer.h

--- ConsoleApplication2/CComputer.h
+++ ConsoleApplication2/CComputer.h
@@ -12,8 +12,6 @@ T input(string s = "")
 	cout << s; cin >> var;
 	return var;
 }
-
-// Преобразователь в строку
 template <typename T>
 std::string toString(T val)
 {
@@ -25,21 +23,17 @@ std::string toString(T val)
 class CComputer
 {
 private:
-	double CPUFreq = 0;			// Частота процессора в ГГц
-	int    NumOfCores = 0;		// Кол-во ядер процессора в ШТ
-	double RAM = 0;				// Объём ОЗУ в ГБ
-	double ROM = 0;				// Объём ПЗУ в ГБ
-	double ROMBusy = 0;			// Занятый объём от объёма ПЗУ в ГБ
-	static double TotalROM; 	// Суммарный объём жёстких всех компьютеров в ГБ
+	double CPUFreq = 0;		
+	int    NumOfCores = 0;		
+	double RAM = 0;			
+	double ROM = 0;			
+	double ROMBusy = 0;		
+	static double TotalROM; 	
 	static double TotalROMBusy;
 public:
-	// Конструктор
 	CComputer(double = 0, int = 0, double = 0, double = 0, double = 0);
-
-	// Деструктор
 	~CComputer();
 
-	// Блок свойств полей класса
 	double GetCPUFreq();
 	void SetCPUFreq(double freq);
 
@@ -55,24 +49,16 @@ public:
 	double GetROMBusy();
 	void SetROMBusy(double romb);
 
-	// Получение свободного объёма ПЗУ
 	double GetROMFree();
 
-	// Проверить возможность добавления файла определённого размера
 	bool TryCopyFile(double file);
 
-	// Вывод информации о компьютере
 	string Info();
-
-	// Внесение изменеий в жёсткий диск
 	void UpdateROM(double file);
 	void UpdateROM(double file, double up_rom);
 
-	// Перегрузка операторов больше и меньше для сравнения кол-ва ядер
 	bool operator > (CComputer pc);
 	bool operator < (CComputer pc);
-
-	// Получение общего объёмов жёсткого диска
 	static double GetTotalROM();
 	static double GetTotalROMBusy();
 	static double GetTotalROMFree();
