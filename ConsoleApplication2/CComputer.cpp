9caa0a33282e0f646c8b45549a5c1291a11fdb00
Andrey Gorobtzov <92875957+AndreyGorobtzov@users.noreply.github.com>
2023-11-11 18:56:55 +0500

    Update CComputer.cpp

--- ConsoleApplication2/CComputer.cpp
+++ ConsoleApplication2/CComputer.cpp
@@ -1,8 +1,5 @@
 #include "CComputer.h"
 
-
-
-// Блок свойств полей класса
 double CComputer::GetCPUFreq() 
 {
 	return CPUFreq;
@@ -59,37 +56,31 @@ void CComputer::SetROMBusy(double romb)
 	ROMBusy = romb;
 	TotalROMBusy += romb;
 }
-
-// Получение свободного объёма ПЗУ
 double CComputer::GetROMFree()
 {
 	return ROM - ROMBusy;
 }
-
-// Проверить возможность добавления файла определённого размера
 bool CComputer::TryCopyFile(double file)
 {
 	return (file + ROMBusy <= ROM);
 }
 
-// Вывод информации о компьютере
 string CComputer::Info()
 {
 	return
-	 	  "Частота процессора         : " + toString(CPUFreq)	+ " ГГц\n"
-	 	+ "Кол - во ядер процессора   : " + toString(NumOfCores)+ " ШТ\n" 
-	 	+ "Объём ОЗУ                  : " + toString(RAM)		+ " ГБ\n" 
-	 	+ "Объём ПЗУ                  : " + toString(ROM)		+ " ГБ\n" 
-	 	+ "Занятый объём от объёма ПЗУ: " + toString(ROMBusy)	+ " ГБ";
+	 	  "Г—Г Г±ГІГ®ГІГ  ГЇГ°Г®Г¶ГҐГ±Г±Г®Г°Г          : " + toString(CPUFreq)	+ " ГѓГѓГ¶\n"
+	 	+ "ГЉГ®Г« - ГўГ® ГїГ¤ГҐГ° ГЇГ°Г®Г¶ГҐГ±Г±Г®Г°Г    : " + toString(NumOfCores)+ " ГГ’\n" 
+	 	+ "ГЋГЎГєВёГ¬ ГЋГ‡Г“                  : " + toString(RAM)		+ " ГѓГЃ\n" 
+	 	+ "ГЋГЎГєВёГ¬ ГЏГ‡Г“                  : " + toString(ROM)		+ " ГѓГЃ\n" 
+	 	+ "Г‡Г Г­ГїГІГ»Г© Г®ГЎГєВёГ¬ Г®ГІ Г®ГЎГєВёГ¬Г  ГЏГ‡Г“: " + toString(ROMBusy)	+ " ГѓГЃ";
 }
 
-// Внесение изменеий в жёсткий диск
 void CComputer::UpdateROM(double file)
 {
 	if (TryCopyFile(file))
 		ROMBusy += file;
 	else
-		cout << "Ошибка!!! Недостаточно места на диске";
+		cout << "ГЋГёГЁГЎГЄГ !!! ГЌГҐГ¤Г®Г±ГІГ ГІГ®Г·Г­Г® Г¬ГҐГ±ГІГ  Г­Г  Г¤ГЁГ±ГЄГҐ";
 }
 void CComputer::UpdateROM(double file, double up_rom)
 {
@@ -101,7 +92,7 @@ void CComputer::UpdateROM(double file, double up_rom)
 		TotalROM += up_rom;
 	}
 	else
-		cout << "Ошибка!!! Недостаточно места на диске";
+		cout << "ГЋГёГЁГЎГЄГ !!! ГЌГҐГ¤Г®Г±ГІГ ГІГ®Г·Г­Г® Г¬ГҐГ±ГІГ  Г­Г  Г¤ГЁГ±ГЄГҐ";
 }
 
 bool CompareC(CComputer c1, CComputer c2)
@@ -109,7 +100,6 @@ bool CompareC(CComputer c1, CComputer c2)
 	return c1.GetROMFree() > c2.GetROMFree();
 }
 
-// Перегрузка операторов больше и меньше для сравнения кол-ва ядер
 bool CComputer::operator > (CComputer pc)
 {
 	return NumOfCores > pc.NumOfCores;
@@ -119,7 +109,6 @@ bool CComputer::operator < (CComputer pc)
 	return NumOfCores < pc.NumOfCores;
 }
 
-// Получение общего объёма жёсткого диска
 double CComputer::GetTotalROM()
 {
 	return TotalROM;
@@ -137,14 +126,12 @@ double CComputer::GetTotalROMFree()
 
 void CComputer::CIn()
 {
-	SetCPUFreq(input<double>("Укажите частоту процессора:        "));
-	SetNumOfCores(input<int>("Укажите кол-во ядер процессора:    "));
-	SetRAM(input<double>	("Укажите оперативную память:        "));
-	SetROM(input<double>	("Укажите постоянную память:         "));
-	SetROMBusy(input<double>("Укажите занятую постоянную память: "));
+	SetCPUFreq(input<double>("Г“ГЄГ Г¦ГЁГІГҐ Г·Г Г±ГІГ®ГІГі ГЇГ°Г®Г¶ГҐГ±Г±Г®Г°Г :        "));
+	SetNumOfCores(input<int>("Г“ГЄГ Г¦ГЁГІГҐ ГЄГ®Г«-ГўГ® ГїГ¤ГҐГ° ГЇГ°Г®Г¶ГҐГ±Г±Г®Г°Г :    "));
+	SetRAM(input<double>	("Г“ГЄГ Г¦ГЁГІГҐ Г®ГЇГҐГ°Г ГІГЁГўГ­ГіГѕ ГЇГ Г¬ГїГІГј:        "));
+	SetROM(input<double>	("Г“ГЄГ Г¦ГЁГІГҐ ГЇГ®Г±ГІГ®ГїГ­Г­ГіГѕ ГЇГ Г¬ГїГІГј:         "));
+	SetROMBusy(input<double>("Г“ГЄГ Г¦ГЁГІГҐ Г§Г Г­ГїГІГіГѕ ГЇГ®Г±ГІГ®ГїГ­Г­ГіГѕ ГЇГ Г¬ГїГІГј: "));
 }
-
-// Конструктор
 CComputer::CComputer(double freq, int num, double ram, double rom, double romb)
 {
 	SetCPUFreq(freq);
@@ -153,8 +140,6 @@ CComputer::CComputer(double freq, int num, double ram, double rom, double romb)
 	SetROM(rom);
 	SetROMBusy(romb);
 }
-
-// Деструктор
 CComputer::~CComputer()
 {
 	TotalROM -= ROM;
